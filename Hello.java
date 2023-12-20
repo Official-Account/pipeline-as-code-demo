624a8a3215658a7593999399024f7470dea86e48
Vikas Predhva <vp.official666@gmail.com>
2023-12-20 14:32:13 +0530

    second succsessfull jenkins test-1

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 class Hello {
 		public static void main(String[] args) {
-			System.out.println("Inside hello java file")
+			System.out.println("Inside hello java file");
 			System.out.println("Hello, This is Declarative CICD Pipeline usingin jenkinsfile inside project"); 
 	}
 }
